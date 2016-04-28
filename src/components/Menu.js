2523cb766b92e9b842017d42f6f6c354103ea942
Carlos Montezano <cmontezanol@gmail.com>
2016-04-28 20:01:29 -0300

    put menu inside a toolbar

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import DropDownMenu from 'material-ui/DropDownMenu';
 import MenuItem from 'material-ui/MenuItem';
 import { browserHistory } from 'react-router'
+import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from 'material-ui/Toolbar';
 
 export default class Menu extends Component {
 
@@ -17,12 +18,14 @@ export default class Menu extends Component {
 
   render() {
     return (
-      <DropDownMenu value={this.state.value} onChange={this.handleChange}>
-        <MenuItem value={'/ru'} primaryText="Restaurante Universitário"/>
-        <MenuItem value={'/mu'} primaryText="Restaurante Espaço Multiuso"/>
-        <MenuItem value={'/login'} primaryText="Login/Cadastrar"/>
-        <MenuItem value={'/about'} primaryText="Sobre o app" />
-      </DropDownMenu>
+      <Toolbar>
+        <DropDownMenu value={this.state.value} onChange={this.handleChange}>
+          <MenuItem value={'/ru'} primaryText="Restaurante Universitário"/>
+          <MenuItem value={'/mu'} primaryText="Restaurante Espaço Multiuso"/>
+          <MenuItem value={'/login'} primaryText="Login/Cadastrar"/>
+          <MenuItem value={'/about'} primaryText="Sobre o app" />
+        </DropDownMenu>
+      </Toolbar>
     );
   }
 }
